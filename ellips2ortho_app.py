5189b86b0654cb67ccbebea11cede7b1e8b85fef
irwin-amago <84965828+irwin-amago@users.noreply.github.com>
2022-03-17 01:01:20 +0800

    Update ellips2ortho_app.py

--- ellips2ortho_app.py
+++ ellips2ortho_app.py
@@ -58,7 +58,7 @@ if uploaded_csv is not None:
                   'GEOID18': 14}
     units_dict ={'Meters': 1, 'US Feet': 2}
     
-    geoid_select = st.selectbox('Please Choose Desired Geoid', ('<select>',
+    geoid_select = st.selectbox('Please Select Desired Geoid', ('<select>',
                                                                 'GEOID99',
                                                                 'G99SSS',
                                                                 'GEOID03',
