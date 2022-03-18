70038cc8e40c0420ddf647dbf782a78acd12b5a7
irwin-amago <84965828+irwin-amago@users.noreply.github.com>
2022-03-18 21:01:04 +0800

    Added a batch download option.
    The user can now upload multiple CSVs. They have the option to visualize any of the uploaded files. The script processes each of the files and then zips them together for download.

--- ellips2ortho_app.py
+++ ellips2ortho_app.py
@@ -2,45 +2,64 @@ import pandas as pd
 import requests
 import streamlit as st
 import pydeck as pdk
+import zipfile
 
 
 st.title('Ellipsoidal to Orthometric Heights')
 st.caption('The application uses the NGS Geoid API to look up the geoid height at a particular location and uses this value to then compute the orthometric height based on the desired units of the user.')
 
-uploaded_csv = st.file_uploader('Please Select Geotags CSV.')
+uploaded_csvs = st.file_uploader('Please Select Geotags CSV.', accept_multiple_files=True)
 
-if uploaded_csv is not None:
-    df = pd.read_csv(uploaded_csv, index_col=False)
-    msg = 'Upload Successful: ' + uploaded_csv.name
-    st.success(msg)
+uploaded = False
 
+for uploaded_csv in uploaded_csvs: 
+    if uploaded_csv is not None:
+        uploaded = True
+    else:
+        uplaoded = False
+
+if uploaded:
+    dfs = []
+    filenames = []
+    df_dict = {}
+    ctr = 0
+    for uploaded_csv in uploaded_csvs:
+        df = pd.read_csv(uploaded_csv, index_col=False)       
+        dfs.append(df)
+        df_dict[uploaded_csv.name] = ctr
+        filenames.append(uploaded_csv.name)
+        ctr += 1
+    st.success('All CSCvs uploaded successfully.')
+    
+    options = filenames.copy()
+    options.insert(0, '<select>')
+    option = st.selectbox('Select geotags CSV to visualize', options)
+    
     lat = 'latitude [decimal degrees]'
     lon = 'longitude [decimal degrees]'
     height = 'altitude [meter]'
     
-    # Generate a Map Box for user verification
-    
-    points_df = pd.concat([df[lat], df[lon]], axis=1, keys=['lat','lon'])
-    
-    st.pydeck_chart(pdk.Deck(
-    map_style='mapbox://styles/mapbox/satellite-streets-v11',
-    initial_view_state=pdk.ViewState(
-        latitude=df[lat].mean(),
-        longitude=df[lon].mean(),
-        zoom=14,
-        pitch=0,
-     ),
-     layers=[
-         pdk.Layer(
-             'ScatterplotLayer',
-             data=points_df,
-             get_position='[lon, lat]',
-             get_color='[70, 130, 180, 200]',
-             get_radius=15,
+    if option != '<select>':
+        points_df = pd.concat([dfs[df_dict[option]][lat], dfs[df_dict[option]][lon]], axis=1, keys=['lat','lon'])
+        
+        st.pydeck_chart(pdk.Deck(
+        map_style='mapbox://styles/mapbox/satellite-streets-v11',
+        initial_view_state=pdk.ViewState(
+            latitude=points_df['lat'].mean(),
+            longitude=points_df['lon'].mean(),
+            zoom=14,
+            pitch=0,
          ),
-         ],
-     ))
-
+         layers=[
+             pdk.Layer(
+                 'ScatterplotLayer',
+                 data=points_df,
+                 get_position='[lon, lat]',
+                 get_color='[70, 130, 180, 200]',
+                 get_radius=20,
+             ),
+             ],
+         ))
 
     # Select Geoid Model
 
@@ -58,7 +77,7 @@ if uploaded_csv is not None:
                   'GEOID18': 14}
     units_dict ={'Meters': 1, 'US Feet': 2}
     
-    geoid_select = st.selectbox('Please Select Desired Geoid', ('<select>',
+    geoid_select = st.selectbox('Please Choose Desired Geoid', ('<select>',
                                                                 'GEOID99',
                                                                 'G99SSS',
                                                                 'GEOID03',
@@ -76,88 +95,86 @@ if uploaded_csv is not None:
         st.write('You selected:', geoid_select)
         geoid = geoid_dict[geoid_select]
     
-    # Unit selection
-    
     units_select = st.selectbox('Please Select Desired Units', ('<select>', 'Meters','US Feet'))
     
     if not units_select=='<select>':
         st.write('You selected:', units_select)
         units = units_dict[units_select]
     
-    # Processing button only appears if pre-reqs are achieved
-    
-    if uploaded_csv is not None and not geoid_select=='<select>' and not units_select=='<select>':
+    if uploaded and not geoid_select=='<select>' and not units_select=='<select>':
         if st.button('CONVERT HEIGHTS'):
-            my_bar = st.progress(0)
-            cmd = 'https://geodesy.noaa.gov/api/geoid/ght?'
-            
-            # Request geoid height at each point and compute orthometric height by using H = h - N
-            
-            ortho = []
-            for x in range(len(df[lat])):
-                lat_req = str(df[lat][x])
-                lon_req = str(df[lon][x])
-                ellip = df[height][x]
-                req = cmd + 'lat=' + lat_req + '&lon=' + lon_req + '&model=' + str(geoid)
-                    
-                try:
-                    responseGeoid = requests.get(req)
-                    responseGeoid.raise_for_status()
-                except requests.HTTPError  as errh:
-                    msg = "Error Connecting: " + str(errh)
-                    st.error(msg)
-                    st.stop()
-                except requests.ConnectionError as errc:
-                    msg = "Error Connecting: " + str(errc)
-                    st.error(msg)
-                    st.stop()
-                except requests.Timeout as errt:
-                    msg = "Error Connecting: " + str(errt)
-                    st.error(msg)
-                    st.stop()
-                except requests.RequestException as err:
-                    msg = "Error Connecting: " + str(err)
-                    st.error(msg)
-                    st.stop()
-            
-                my_bar.progress((x+1)/len(df[lat]))     
-                ortho_height = ellip - responseGeoid.json()['geoidHeight']
+            file_ctr = 0
+            for df in dfs:
+                st.text('Processing ' + filenames[file_ctr] + '.')
+                my_bar = st.progress(0)
+                cmd = 'https://geodesy.noaa.gov/api/geoid/ght?'
                 
-                # Unit conversion when needed
+                ortho = []
+                for x in range(len(df[lat])):
+                    lat_req = str(df[lat][x])
+                    lon_req = str(df[lon][x])
+                    ellip = df[height][x]
+                    req = cmd + 'lat=' + lat_req + '&lon=' + lon_req + '&model=' + str(geoid)
+                        
+                    try:
+                        responseGeoid = requests.get(req)
+                        responseGeoid.raise_for_status()
+                    except requests.HTTPError  as errh:
+                        msg = "Error Connecting: " + str(errh)
+                        st.error(msg)
+                        st.stop()
+                    except requests.ConnectionError as errc:
+                        msg = "Error Connecting: " + str(errc)
+                        st.error(msg)
+                        st.stop()
+                    except requests.Timeout as errt:
+                        msg = "Error Connecting: " + str(errt)
+                        st.error(msg)
+                        st.stop()
+                    except requests.RequestException as err:
+                        msg = "Error Connecting: " + str(err)
+                        st.error(msg)
+                        st.stop()
                 
+                    my_bar.progress((x+1)/len(df[lat]))     
+                    ortho_height = ellip - responseGeoid.json()['geoidHeight']
+                        
+                    if units==1:
+                        ortho.append(ortho_height)
+                    else:
+                        ortho.append(ortho_height*3.2808399)
+        
+                df[height] = ortho
+                file_ctr += 1
                 if units==1:
-                    ortho.append(ortho_height)
+                    df.rename(columns={height: 'orthometric height [meter]'}, inplace=True)
                 else:
-                    ortho.append(ortho_height*3.2808399)
-    
-            # Save the new heights in the dataframe, rename columns, and adjust units if needed
-    
-            df[height] = ortho
-            if units==1:
-                df.rename(columns={height: 'orthometric height [meter]'}, inplace=True)
-            else:
-                df['accuracy horizontal [meter]'] = df['accuracy horizontal [meter]'].apply(lambda x: x*3.2808399)
-                df['accuracy vertical [meter]'] = df['accuracy vertical [meter]'].apply(lambda x: x*3.2808399)
-                df.rename(columns={height: 'orthometric height [feet]',
-                                   'accuracy horizontal [meter]': 'accuracy horizontal [feet]', 
-                                   'accuracy vertical [meter]': 'accuracy vertical [feet]'}, inplace=True)
+                    df['accuracy horizontal [meter]'] = df['accuracy horizontal [meter]'].apply(lambda x: x*3.2808399)
+                    df['accuracy vertical [meter]'] = df['accuracy vertical [meter]'].apply(lambda x: x*3.2808399)
+                    df.rename(columns={height: 'orthometric height [feet]',
+                                       'accuracy horizontal [meter]': 'accuracy horizontal [feet]', 
+                                       'accuracy vertical [meter]': 'accuracy vertical [feet]'}, inplace=True)
             
-            st.success('Height conversion finished. Click button below to download new CSV.')
+            st.success('Height conversion finished. Click button below to download converted files.')
             
-            # Save the new dataframe as a CSV and prepare for user download
             
-            def convert_df(df):
-                 return df.to_csv(index=False).encode('utf-8')
+            # Create the zip file and convert the dataframes to CSV
             
-            csv = convert_df(df)
-            filename = uploaded_csv.name.split('.')[0] + '_orthometric.csv'
+            with zipfile.ZipFile('Converted_CSV.zip', 'w') as csv_zip:
+                file_ctr = 0
+                for df in dfs:
+                    csv_zip.writestr(filenames[file_ctr].split('.')[0] + '_orthometric.csv', df.to_csv(index=False).encode('utf-8'))
+                    file_ctr += 1   
             
+            # Download button for the zipped CSVs
+            
+            fp = open('Converted_CSV.zip', 'rb')
             st.download_button(
-                 label="Download Converted Geotags CSV",
-                 data=csv,
-                 file_name=filename,
-                 mime='text/csv',
-             )
+                label="Download Converted Geotags CSV",
+                data=fp,
+                file_name='Converted_CSV.zip',
+                mime='application/zip',
+            )
     st.stop()
 else:
     st.stop()
